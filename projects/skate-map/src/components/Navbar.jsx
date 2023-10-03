98f303037990d0db9edc9b7d0f351e98f7cb6c13
Mae Dae <MaeDae>
2023-10-03 13:09:44 -0700

    more renaming

--- projects/skate-map/src/components/Navbar.jsx
+++ projects/skate-map/src/components/Navbar.jsx
@@ -7,11 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavScroll() {
   return (
-    <Navbar bg="light" expand="lg" className="fs-4 flex-grow-1 px-5">
+    <Navbar bg="dark" expand="lg" className="fs-4 flex-grow-1 px-5" style={{backgroundColor: 'black', color: 'white'}}>
       
-        <Navbar.Brand href="#">Queer Skate Map</Navbar.Brand>
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Login</Nav.Link>
+        <Navbar.Brand href="#" style={{color:'white'}}>Queer Skate Map</Navbar.Brand>
+            <Nav.Link href="#action1" style={{color:'white', padding:'1em'}}>Home</Nav.Link>
+            <Nav.Link href="#action2" style={{color:'white'}}>Login</Nav.Link>
       
     </Navbar>
   );
